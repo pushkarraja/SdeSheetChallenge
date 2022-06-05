1ab288eecbea7c288759e39b873fdfbfd50e62eb
Pushkar R <47273789+pushkarraja@users.noreply.github.com>
2022-06-05 23:34:50 +0530

    Create Merge Sorted Array.java

---
+++ Day - 2/Merge Sorted Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = nums1.length - 1;
+        
+        while (j >= 0 && i >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i];
+                i--;
+            } else {
+                nums1[k--] = nums2[j];
+                j--;
+            }
+        }
+        
+        while (i >= 0) {
+            nums1[k--] = nums1[i--];
+        }
+        
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
