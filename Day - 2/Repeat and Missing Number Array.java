385f52219c143055d838f28e0d1f0f9ff8791229
Pushkar R <47273789+pushkarraja@users.noreply.github.com>
2022-06-05 23:37:55 +0530

    Create Repeat and Missing Number Array.java

---
+++ Day - 2/Repeat and Missing Number Array.java
@@ -0,0 +1,39 @@
+static int x, y;
+ 
+    static void getTwoElements(int arr[], int n)
+    {
+
+        int xor1;
+
+        int set_bit_no;
+ 
+        int i;
+        x = 0;
+        y = 0;
+ 
+        xor1 = arr[0];
+ 
+        for (i = 1; i < n; i++)
+            xor1 = xor1 ^ arr[i];
+ 
+        for (i = 1; i <= n; i++)
+            xor1 = xor1 ^ i;
+ 
+        set_bit_no = xor1 & ~(xor1 - 1);
+
+        for (i = 0; i < n; i++) {
+            if ((arr[i] & set_bit_no) != 0)
+                x = x ^ arr[i];
+ 
+            else
+                y = y ^ arr[i];
+        }
+        for (i = 1; i <= n; i++) {
+            if ((i & set_bit_no) != 0)
+                x = x ^ i;
+ 
+            else
+                y = y ^ i;
+        }
+        
+    }
