60bd6631a7d29e27bb5f9783c579cf981ccfc149
Pushkar R <47273789+pushkarraja@users.noreply.github.com>
2022-06-05 23:35:38 +0530

    Create Duplicate Number.java

---
+++ Day - 2/Duplicate Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[Math.abs(nums[i])] < 0) {
+                return Math.abs(nums[i]);
+            } else {
+                nums[Math.abs(nums[i])] = -nums[Math.abs(nums[i])];
+            }
+        }
+        
+        return -1;
+    }
+}
