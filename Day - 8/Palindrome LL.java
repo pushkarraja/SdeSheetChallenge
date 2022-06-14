d2a0250f3eb558560ae73e35f2a397200dba77cf
Pushkar R <47273789+pushkarraja@users.noreply.github.com>
2022-06-15 00:08:40 +0530

    Create Palindrome LL.java

---
+++ Day - 8/Palindrome LL.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+         if(head == null){
+            return true;
+        }
+        
+        ListNode mid = middle(head);
+        ListNode last = reverse(mid);
+        ListNode curr = head;
+        while(last != null && curr != null){
+            if(last.val != curr.val){
+                return false;
+            }
+            last = last.next;
+            curr = curr.next;
+        }
+        return true;
+    }
+    
+   public ListNode reverse(ListNode m){
+        ListNode curr = m;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+    
+    public ListNode middle(ListNode m){
+        ListNode fast = m;
+        ListNode slow = m;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
